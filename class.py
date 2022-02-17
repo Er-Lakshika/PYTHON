4a337d1ae5f79611890ae1bbf9e17bfc0656ed81
Er. Lakshika <85821865+Er-Lakshika@users.noreply.github.com>
2022-02-17 18:58:33 +0530

    git push class.py git push functions.py git push rectangle.py

---
+++ class.py
@@ -0,0 +1,21 @@
+from _typeshed import Self
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x= x
+        self.y= y
+
+p = Point(2,8)
+print(p.x)
+print(p.y)
+
+class list():
+    def __init__(self,a,b,c,f):
+        self.a= dfg
+        self.b=mnb
+        self.c= Lakshika
+        self.f= Raghav
+
+s = list{a,b,c,d,e}
+print(S.c)
---
+++ functions.py
@@ -0,0 +1,7 @@
+from rectangle.py import area
+
+for i in range (5,5) :
+    x= input ("x is equal to")
+    y= input("y is equal to")
+
+    print("Area of rectangle is" + area)
